0cd1baeecdc0577b99f7253fd4d318e5906b5d33
Yok0016 <supakon8898@gmail.com>
2026-05-11 14:47:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
-const CACHE = 'rice-v1';
+const CACHE = 'rice-v4';
 const FILES = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
 });
 
 self.addEventListener('fetch', e => {
