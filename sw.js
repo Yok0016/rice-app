8b8b9c5e3d9c7a277a1765859b6c4594373c807e
Yok0016 <supakon8898@gmail.com>
2026-05-12 14:53:20 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,8 @@
-const CACHE = 'rice-v4';
+const CACHE = 'rice-v1';
 const FILES = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
 });
 
 self.addEventListener('fetch', e => {
